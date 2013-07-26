cb667f3b7d059ed1f6a65223714070b28013d77c
bendyorke <benyorke@me.com>
2013-07-25 22:18:39 -0500

    added successfully API querying

--- semantic_parser.rb
+++ semantic_parser.rb
@@ -1,9 +1,27 @@
 module SemanticParser
+	require 'open-uri'
+	require 'json'
 
-	
+	API_BASE = "https://www.googleapis.com/language/translate/v2?key=" + ENV['GAPI']
+
+	def api_call query, source, target
+		api_call = API_BASE + "&q=" + query + "&source=#{source}&target=#{target}"
+		api_hit = open(api_call)
+		JSON.parse(api_hit.read)
+	end
+
+
 
 end
 
 if $0 == __FILE__
-	puts 'hello'
+	include SemanticParser
+
+
+
+	puts api_call "words", "en", "fr"
+
+	sampleInput 		= Content.new(:value => "")
+	sampleResponse 	= Content.new(:value => "")
+
 end
