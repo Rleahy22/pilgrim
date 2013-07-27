c15c7251b267e596d2a52a685105ad869e270b50
bendyorke <benyorke@me.com>
2013-07-27 16:58:51 -0500

    Test json returned

--- app/models/semantic_parser.rb
+++ app/models/semantic_parser.rb
@@ -1,7 +1,4 @@
 # encoding: UTF-8
-require 'open-uri'
-require 'json'
-API_BASE = "https://www.googleapis.com/language/translate/v2?key=" + ENV['GAPI']
 
 class SemanticParser
   attr_reader :translation, :nodes, :corr
@@ -67,25 +64,12 @@ class SemanticParser
   end
 end
 
-class Translator
-	def self.fetch(query, source, target)
-	#	api_call 		= API_BASE + "&q=" + query + "&source=#{source}&target=#{target}" 
-	#	api_hit 		= open(api_call)
-	#	api_return 	= JSON.parse(api_hit.read)
-	#	api_return['data']['translations'][0]['translatedText']
-	#	scentences_in 	= paragraph_in.split('.').strip.map { |scen| URI.escape(scen) }
-	#	scentences_back = scentences_in.map { |scen| api_call scen, source, target }
-    raise "Tried to hit API" 
-	end
-end
 
-if $0 == __FILE__
-	G_INPUT 			= "Ultra High Definition TV technology offers resolution measuring 3840 x 2160 pixels, or 8 megapixels -- four times that of 1080p televisions, which offer 2 megapixels of resolution.Besides far higher resolution, one advantage pointed out by industry pundits is that 4K TV can show passive 3D better than today's 1080p sets."
-	G_RESPONSE 	= "La technologie de TV haute définition Ultra offre mesurant 3840 x 2160 pixels, soit 8 mégapixels résolution - quatre fois celle des téléviseurs 1080p, offrant 2 mégapixels de résolution. D'ailleurs beaucoup plus haute résolution, un avantage souligné par les experts de l'industrie est que TV 4K 3D passive peut montrer mieux que jeux 1080p d'aujourd'hui."
-	G_VERIFY			= ["the", "technology", "of", "TV", "high", "definition", "ultra", "offers", "measuring", "3840", "x", "2160", "pixels", "or", "8", "megapixel", "resolution", "-", "four", "times", "that", "of", "TVs", "1080p", "offering", "2", "megapixel", "of", "resolution", "Indeed", "a lot", "more", "high", "resolution", "a", "advantage", "out", "by", "the", "experts", "of", "industry", "is", "that", "TV", "4K", "3D", "passive", "can", "show", "more", "that", "games", "1080p", "today"]
+# if $0 == __FILE__
+# 	G_INPUT 			= "Ultra High Definition TV technology offers resolution measuring 3840 x 2160 pixels, or 8 megapixels -- four times that of 1080p televisions, which offer 2 megapixels of resolution.Besides far higher resolution, one advantage pointed out by industry pundits is that 4K TV can show passive 3D better than today's 1080p sets."
+# 	G_RESPONSE 	= "La technologie de TV haute définition Ultra offre mesurant 3840 x 2160 pixels, soit 8 mégapixels résolution - quatre fois celle des téléviseurs 1080p, offrant 2 mégapixels de résolution. D'ailleurs beaucoup plus haute résolution, un avantage souligné par les experts de l'industrie est que TV 4K 3D passive peut montrer mieux que jeux 1080p d'aujourd'hui."
+# 	G_VERIFY			= ["the", "technology", "of", "TV", "high", "definition", "ultra", "offers", "measuring", "3840", "x", "2160", "pixels", "or", "8", "megapixel", "resolution", "-", "four", "times", "that", "of", "TVs", "1080p", "offering", "2", "megapixel", "of", "resolution", "Indeed", "a lot", "more", "high", "resolution", "a", "advantage", "out", "by", "the", "experts", "of", "industry", "is", "that", "TV", "4K", "3D", "passive", "can", "show", "more", "that", "games", "1080p", "today"]
 
-	parser = SemanticParser.new(nil, nil, nil, {:source => G_INPUT.split, :target => G_RESPONSE.split, :verify => G_VERIFY})
-	#parser.nodes.keys.each { |k| p parser.nodes[k] }
-  parser.parse
-	#puts parser.results
-end
+# 	parser = SemanticParser.new(nil, nil, nil, {:source => G_INPUT.split, :target => G_RESPONSE.split, :verify => G_VERIFY})
+#   parser.parse
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pilgrim::Application.routes.draw do
   resources :words
   root :to => 'words#index'
+  get '/test' => 'parsers#test'
 end
