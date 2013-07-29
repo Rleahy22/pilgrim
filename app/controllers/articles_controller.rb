01ad75b0889b687c1e0ffe5f5bf7dafc87bf069a
Tyler Shipe <tjshipe@gmail.com>
2013-07-29 11:42:03 -0500

    updated articles controllers to display multiple articles and indiv articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,14 @@ require 'htmlentities'
 
 class ArticlesController < ApplicationController
   def index
-    @article = Article.first
+    @articles = Article.all
+    # coder = HTMLEntities.new
+    # formatted = coder.decode(@article.translatable)
+    # @formatted = formatted.split("|&")
+  end
+
+  def show
+    @article = Article.find(params[:id])
     coder = HTMLEntities.new
     formatted = coder.decode(@article.translatable)
     @formatted = formatted.split("|&")
