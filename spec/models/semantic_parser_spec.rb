29c2240f9632a9a76239a28efd40725153a9e52b
bendyorke <benyorke@me.com>
2013-07-30 15:29:07 -0500

    semantic_parser refactored, all green with new tests

--- spec/models/semantic_parser_spec.rb
+++ spec/models/semantic_parser_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 describe SemanticParser do
-  let(:lazy) { "the quick brown fox jumps over the lazy dog" }
-  let(:paresseux) do 
+  let(:parser) do 
     SemanticParser.new(nil, nil, nil, {
       :source => ["the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog", "quick"], 
       :target => ["Le", "rapide", "renard", "brun", "saute", "sur", "le", "chien", "paresseux", "rapide"], 
@@ -12,51 +11,63 @@ describe SemanticParser do
 
   describe "#initialize" do
     it "hits API without a translated source" do
-      expect { SemanticParser.new(nil, nil, nil, nil) }.to raise_error
+      SemanticParser.any_instance.stub(:translate) { {source: nil, target: nil, verify: nil} }
+      SemanticParser.any_instance.should_receive(:translate)
+      SemanticParser.new(nil, nil, nil, nil)
     end
 
     it "avoids translation with a translated source" do
-      expect { SemanticParser.new(nil, nil, nil, {})  }.not_to raise_error
+      SemanticParser.any_instance.stub(:translate)
+      SemanticParser.any_instance.should_not_receive(:translate)
+      SemanticParser.new(nil, nil, nil, {})
     end
 
-    it "creates nodes as a Hash" do
-      expect(paresseux.nodes.class).to eq Hash
-    end
-
-    it "sets translation to a 3 ket hash" do
-      expect(paresseux.translation.keys.length).to eq 3
+    it "passes correct values to the translate function" do
+      SemanticParser.any_instance.stub(:translate) { {source: nil, target: nil, verify: nil} }
+      SemanticParser.any_instance.should_receive(:translate).with("en", "fr")
+      SemanticParser.new(nil, "en", "fr")
     end
   end
   
   describe "#parse" do
-    it "removes all duplicates" do
-      paresseux.parse
-      expect(paresseux.corr.values.map(&:class).uniq).to eq [Fixnum]
-    end
-
-    it "returns a hash" do
-      expect(paresseux.parse.class).to eq Hash 
-    end
-
-    it "provides a lvl 1 for all elements" do
+    it "returns an array of hashes" do
+      expect(parser.parse).to eq parser.json
     end
   end
 
-  describe "@corr" do
-    it "filters the results down to their reverse translation" do
-      paresseux.parse
-      expect(paresseux.corr.keys.count).to eq 9
+  describe "#correlate_words" do
+    it "can find multiple matches" do
+      parser.correlate_words
+      expect(parser.correlated_words.values.map(&:length)).to include 2 
     end
   end
 
-  describe "@nodes" do
-    it "contains a source, target, and verify key" do
-      expect(paresseux.nodes.keys).to eq [:source, :target, :verify]
-    end
-
-    it "has hash-class values" do
-      expect(paresseux.nodes.values[0].class).to eq Hash
+  describe "#select_correct_correlation" do
+    it "leaves no words with multiple matches" do
+      parser.correlate_words
+      parser.select_correct_correlation
+      expect(parser.correlated_words.values.map(&:class).uniq).to eq [Fixnum]
     end
   end
 
+  describe "#generate_json" do
+    it "returns a hash" do 
+      parser.parse
+      expect(parser.json.class).to  eq Hash
+    end
+
+    it "assigns atleast one word to each index" do
+      parser.parse
+      expect(parser.json.values.map { |word| word["source"].to_s }).not_to include ""
+    end
+  end
+
+  describe "#assign_proficiency_levels" do
+    it "assigns values over 9000!!" do
+      expect(parser.parse.values.map { |word| word["level"] }).to include 9001
+    end
+
+    it "evenly distributes levels" do
+    end
+  end
 end
