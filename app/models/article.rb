908e81ad24e267b7d5c2a7d6fe6687b223a7c770
Tyler Shipe <tjshipe@gmail.com>
2013-07-30 13:18:46 -0500

    removed parsing functionality from article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,34 +1,5 @@
-require 'open-uri'
-
 class Article < ActiveRecord::Base
-  attr_accessible :translatable, :content, :image, :title, :url
+  require 'open-uri'
 
-  before_save :parse
-
-  def self.get_recent_article_urls(rss_url)
-    feed = Feedzirra::Feed.fetch_and_parse(rss_url)
-    feed.entries.map { |entry| entry.url }
-  end
-
-  def self.get_json_content(urls)
-    urls.map do |url|
-      api_format = "https://www.readability.com/api/content/v1/parser?url=#{url}&token=ee8e522664d780a6cd208df1d21fa424f7fe400d"
-      JSON.parse(open(api_format).read)
-    end
-  end
-
-  private
-
-  # moderately janky code below
-
-  def parse
-    split_html = self.content.split(/(<\/?[^>]*>)/)
-    stripped = split_html.map { |el| el.strip }
-    summary = stripped.reject { |el| el.match(/<\/?[^>]*>/) }
-    self.summary = summary.join(' ')
-
-    stripped.reject! { |el| stripped.empty? }
-    words_split = stripped.map { |phrase| phrase.match(/<\/?[^>]*>/) ? phrase : phrase.split(' ') }
-    self.translatable = words_split.flatten!.join("|&")
-  end
+  attr_accessible :translatable, :content, :image, :title, :url, :summary
 end
