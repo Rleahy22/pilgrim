2405693a0e2a32161b430081d3780f3905b9369b
Tyler Shipe <tjshipe@gmail.com>
2013-07-31 11:00:39 -0500

    update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130729132333) do
+ActiveRecord::Schema.define(:version => 20130730154144) do
 
   create_table "articles", :force => true do |t|
     t.text     "title"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(:version => 20130729132333) do
     t.datetime "updated_at",   :null => false
   end
 
+  create_table "translations", :force => true do |t|
+    t.integer  "article_id"
+    t.text     "source_text"
+    t.text     "target_translation"
+    t.text     "reverse_translation"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
+  end
+
   create_table "users", :force => true do |t|
     t.string   "uid"
     t.string   "email"
@@ -31,12 +40,6 @@ ActiveRecord::Schema.define(:version => 20130729132333) do
     t.string   "family_name"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-  end
-
-  create_table "words", :force => true do |t|
-    t.string   "entry"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
 end
